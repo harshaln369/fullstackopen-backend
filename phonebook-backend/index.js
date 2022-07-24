06b697130c0d4a19da13ad6f24a85acb5b1aef40
harshal <harshaln369@gmail.com>
2022-07-24 21:51:06 +0530

    3.3 and 3.4 complete

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.json());
 
-const persons = [
+let persons = [
   {
     id: 1,
     name: "Arto Hellas",
@@ -29,6 +29,24 @@ const persons = [
 
 app.get("/api/persons", (request, response) => {
   response.json(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
 });
 
 app.get("/info", (request, response) => {
