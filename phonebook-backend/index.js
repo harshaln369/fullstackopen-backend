f69bfc77efbc2ff6fa979f8a0bb1e7a9dce1f6b8
harshal <harshaln369@gmail.com>
2022-07-25 21:24:29 +0530

    heroku deploy

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -103,5 +103,5 @@ app.get("/info", (request, response) => {
   );
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log("listening on port: ", PORT));
