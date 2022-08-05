b15b9eee9575d5357c99a2842fb13a8c296f7602
harshal <harshaln369@gmail.com>
2022-08-05 21:14:33 +0530

    4.4 and 4.5 complete

--- blog-list/utils/list_helper.js
+++ blog-list/utils/list_helper.js
@@ -3,6 +3,27 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((total, blog) => {
+    return total + blog.likes
+  }, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  return blogs.reduce(
+    (maxBlog, blog) => {
+      if (maxBlog.likes > blog.likes) {
+        return maxBlog
+      } else {
+        return blog
+      }
+    },
+    { likes: 0 }
+  )
+}
+
 module.exports = {
   dummy,
+  totalLikes,
+  favoriteBlog,
 }
